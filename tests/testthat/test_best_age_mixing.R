3b72caa2b3721bbcbefdc02e3df0673d9e51bc4c
caleb <caleb.easterly@gmail.com>
2018-06-10 20:50:22 -0500

    add actual test

--- tests/testthat/test_best_age_mixing.R
+++ tests/testthat/test_best_age_mixing.R
@@ -5,4 +5,6 @@ source("common_test_defs.R")
 
 test_that("best_age_mixing runs without error", {
     out <- best_age_mixing(mixing_data, start_ages)
+    # was sampled from normal, so it's good that we get normal
+    expect_true(out$all_AIC[, 1][which.min(out$all_AIC[, 3])] == 'normal')
 })
