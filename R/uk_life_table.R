13c8f5be21a85309ece3541128d7c821fd696cf4
caleb <caleb.easterly@gmail.com>
2018-06-10 20:50:38 -0500

    documentation

--- R/uk_life_table.R
+++ R/uk_life_table.R
@@ -1,7 +1,7 @@
 #' Life table data from UK, 2010 - 2012
 #' @docType data
 #' 
-#' @source https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/datasets/nationallifetablesunitedkingdomreferencetables 
+#' @source \url{https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/datasets/nationallifetablesunitedkingdomreferencetables}
 #' 
 #' 
 "uk_life_table"
