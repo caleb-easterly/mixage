48db0d812a70f7c02590934b921e3a78699a4ea5
caleb <caleb.easterly@gmail.com>
2018-07-09 16:38:17 -0400

    remove death prob option

--- R/avg_group_age.R
+++ R/avg_group_age.R
@@ -5,30 +5,29 @@
 # proportion of the population with each (single-year) age
 # if not provided, is calculated from U.K. life tables
 avg_group_age <- function(age_group_list_indices, full_age_distribution = NULL) {
-  n_age <- length(age_group_list_indices)
-  mean_ages <- vector(length = n_age)
-  
-  # minimum age that is modeled
-  min_age <- min(unlist(age_group_list_indices))
-  
-  # maximum age that is modeled
-  max_model_age <- max(unlist(age_group_list_indices))
-  
-  # calculate full age distribution for all ages in population
-  
-  if (is.null(full_age_distribution)){
-      full_age_distribution <- calculate_single_year_age_distribution(min_age, max_model_age)
-  }
+    n_age <- length(age_group_list_indices)
+    mean_ages <- vector(length = n_age)
 
-  # for proper indexing, subtract (min_age - 1) from
-  # age group indices to make min_age have an index of  1
+    # minimum age that is modeled
+    min_age <- min(unlist(age_group_list_indices))
 
-  index_shift <- min_age - 1
-  
-  for (i in 1:n_age){
+    # maximum age that is modeled
+    max_model_age <- max(unlist(age_group_list_indices))
+
+    # calculate full age distribution for all ages in population
+
+    if (is.null(full_age_distribution)){
+        full_age_distribution <- calculate_single_year_age_distribution(min_age, max_model_age)
+    }
+
+    # for proper indexing, subtract (min_age - 1) from
+    # age group indices to make min_age have an index of  1
+    index_shift <- min_age - 1
+
+    for (i in 1:n_age){
     mean_ages[i] <- age_group_list_indices[[i]] %*%
                       full_age_distribution[c(age_group_list_indices[[i]] - index_shift)]/
                       sum(full_age_distribution[c(age_group_list_indices[[i]] - index_shift)])
-  }
-  return(mean_ages)
+    }
+    return(mean_ages)
 }
--- R/calculate_single_year_age_distribution.R
+++ R/calculate_single_year_age_distribution.R
@@ -1,77 +1,52 @@
 #' Calculate age distribution.
 #' @description
-#' If you have annual probabilities of death (from life tables),
-#' you can use this to calculate an age distribution for \link[mixage]{estimate_age_mixing}
+#' Calculate an age distribution from annual probabilities of death (from life tables).
 #'
 #' @param min_age 
 #' minimum model age
 #'
-#' @param max_age
+#' @param max_model_age
 #' maximum model age
 #' 
-#' @param death_prop
-#' Optional: a length 99 vector of annual probabilities of death for 1-year-olds to 99-year-olds
-#' 
 #' @return \code{age_prop}: the proportion of the model population with each age
-#' @export
-calculate_single_year_age_distribution <- function(min_age, max_model_age, death_prob = NULL){
+calculate_single_year_age_distribution <- function(min_age, max_model_age){
     single_year_ind <- seq(min_age, max_model_age)
-    
+
     # calculate start ages from list
     n_age <- length(single_year_ind)
-    
-    # if user does not provide death rates, use built in (uk life tables)
-    if (is.null(death_prob)){
-        # import life-table data
-        # probabilities of death for age 1 to 99
-        # created so that 12th entry is age 12 to 13, for example
 
-        #probabilities of death for age 1 to 100
-        #created so that 12th entry is age 12 to 13
-        mprob <- as.numeric(uk_life_table$mxM[-1])
-        fprob <- as.numeric(uk_life_table$mxF[-1])
-        
-        ### convert probabilities of death to rates
-        #create vector of death rates, mu
-        muM <- death_rate(death_prob = mprob, indices = single_year_ind)
-        muF <- death_rate(death_prob = fprob, indices = single_year_ind)
-        
-        # get the average of male and female rates
-        mu <- rowMeans(cbind(muM, muF))
-        
-    } else {
-        print("Using user-supplied death probabilities. Must be annual death probabilities for age 1, 2, ..., 100")
-        # check length of death prob
-        if (length(death_prob) != 99){
-            stop("vector of death probabilities does not have length 99")
-        }
-        names(death_prob) <- as.character(1:99)
-        
-        ### convert probabilities of death to rates
-        #create vector of death rates, mu
-        mu <- death_rate(death_prob = death_prob, indices = single_year_ind)
-        
-    }
-    
+    # import life-table data
+    #probabilities of death for age 1 to 100
+    #created so that 12th entry is age 12 to 13
+    mprob <- as.numeric(uk_life_table$mxM[-1])
+    fprob <- as.numeric(uk_life_table$mxF[-1])
+
+    ### convert probabilities of death to rates
+    #create vector of death rates, mu
+    muM <- death_rate(death_prob = mprob, indices = single_year_ind)
+    muF <- death_rate(death_prob = fprob, indices = single_year_ind)
+
+    # get the average of male and female rates
+    mu <- rowMeans(cbind(muM, muF))
 
     ## aging rate calculations ####
-    
+
     # bandwidths (number of years in each age group) is 1
     # because these are single years
     age_bandwidths <- rep(1, n_age)
-    
+
     # d is aging rate 
-    d <- transfer_rate(mu, age_bandwidths)
-    
+    d <- aging_rate(mu, age_bandwidths)
+
     ### calculate proportion of population in each year of age ####
-    
+
     #initialize age_proportions vector
     age_prop <- rep(0, n_age)
-    
+
     #using Elbasha, et al. 2007, Appendix, pg. 10
     # explicit translation of first formula on pg. 10
     # calculating youngest age group
-    
+
     # accumulators
     acc <- 0
     inner_acc <- 1
@@ -83,10 +58,10 @@ calculate_single_year_age_distribution <- function(min_age, max_model_age, death
         acc <- acc + inner_acc
         inner_acc <- 1
     }
-    
+
     #calculate proportion in first age group
     age_prop[1] <- 1 / (1 + acc)
-    
+
     # calculate proportions in other age groups
     for (ag in 2:n_age) {
         age_prop[ag] <- d[ag - 1] * age_prop[ag - 1]  / (d[ag] + mu[ag])
